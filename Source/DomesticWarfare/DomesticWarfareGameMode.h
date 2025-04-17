417e50f7ffcb7bce4771ccf64d127effb8315667
Gabriel <gabrielmenezes0297@gmail.com>
2025-04-17 20:53:09 +0100

    Add initial setup for DomesticWarfare project
    Included new game module with dependencies and initial configurations. Added character class with movement, camera, and input systems using Enhanced Input framework. Created default project settings, input mappings, build files, and a .gitignore to organize development.

---
+++ Source/DomesticWarfare/DomesticWarfareGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DomesticWarfareGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ADomesticWarfareGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ADomesticWarfareGameMode();
+};
+
+
+
